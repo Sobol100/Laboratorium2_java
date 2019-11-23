d7772f833afd49a1bdd9484b16799676af4e93a0
Karol Sobisz <karolsobisz@gmail.com>
2019-11-23 17:04:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/laboratorium2_java/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.laboratorium2_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener( v -> {
+                Intent intent = new Intent(this, Main2Activity.class);
+                startActivity(intent);
+            });
+        }
+}
+
