d558b2b70ab1019898597b7970b5adabb8d98cd5
Karol Sobisz <karolsobisz@gmail.com>
2020-01-25 21:42:17 +0100

    fixMyAndroidAPp

--- app/src/main/java/com/example/laboratorium2_java/MainActivity.java
+++ app/src/main/java/com/example/laboratorium2_java/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
+        button = findViewById(R.id.przyciskLogowania);
 
-        button.setOnClickListener( v -> {
-                Intent intent = new Intent(this, Main2Activity.class);
-                startActivity(intent);
-            });
-        }
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AbsenceActivity.class);
+            startActivity(intent);
+        });
+    }
 }
-
